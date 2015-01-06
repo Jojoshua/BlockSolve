61e6b5c0adeba0ae66754f6ef17cadd624e6e755
Jojoshua <Jojoshua@gmail.com>
2015-01-06 16:22:20 -0500

    Correct Duplicate Detection

--- BlockSolve.rs
+++ BlockSolve.rs
@@ -5,7 +5,8 @@ use std::collections::HashSet;
 use std::collections::HashMap;
 
 fn main(){
-	let mut unique_sets = HashSet::new();
+	let mut all_sets: Vec<HashSet<uint>> = vec![];
+	let mut unique_sets: Vec<&HashSet<uint>> = vec![];
 	let mut main_map = HashMap::new();
 	load_input(&mut main_map);
 	
@@ -13,10 +14,8 @@ fn main(){
 		println!("Origial sequence {}", a );	
 	}
 	
-	let mut a_count: uint = 0;	
-	let mut block_count = 0u;
-	for (a_k, a_v) in main_map.iter(){
-		unique_sets.insert(a_k);
+	let mut a_count: uint = 0;		
+	for (a_k, a_v) in main_map.iter(){		
 		a_count += 1;
 		//println!("\n A: {}, {}", a_k , a_v );
 			
@@ -29,15 +28,56 @@ fn main(){
 			} */
 			let intersection : HashSet<uint> = a_v.intersection(b_v).map(|&x| x).collect();
 			if intersection.len() > 1{
-				println!("{} {} {}", a_k, b_k, intersection );
-				block_count +=1;
+				println!("{} {} {}", a_k, b_k, intersection );				
+				all_sets.push(intersection);
 			}						
 		}
 	}
 
-	println!("\n Number of blocks {}", block_count );
+/* 	let mut duplicate = HashSet::new();
+	duplicate.insert(25);
+	duplicate.insert(39);
+	duplicate.insert(30);
+	duplicate.insert(47);	
+	all_sets.push(duplicate);
+	duplicate = HashSet::new();
+	duplicate.insert(19);
+	duplicate.insert(18);
+	duplicate.insert(33);
+	duplicate.insert(4);	
+	duplicate.insert(6);	
+	all_sets.push(duplicate); */
+
 	
-	println!("Number of Unique Blocks {}", unique_sets.len() );
+	println!("\nNumber of blocks {}", all_sets.len() );	
+		
+	//Get unique blocks
+	let mut dup = false;
+	let mut set_count = 0u;
+	a_count = 0;
+	for a in all_sets.iter(){	
+		a_count += 1;	
+		set_count = 0;
+		dup = false;
+		for b in all_sets.iter().skip(a_count){			
+			if a == b{
+				println!("Found Duplicate {} at index {}", b, set_count + a_count );
+				dup = true;	
+				break;
+			}				
+			set_count += 1;			
+		}
+		
+		if	dup == false{
+			unique_sets.push(a);
+		}
+	}	
+ 	println!("\nNumber of Unique Blocks {}", unique_sets.len() );
+	
+/* 	for n in unique_sets.iter(){
+		println!("Unique Block {}", n );	
+	}  */
+	
 	
 }
 
@@ -65,8 +105,8 @@ fn load_input(main_map: &mut HashMap<String,HashSet<uint>>){
 		split_line_vec.remove(0);
 				
 		for i in split_line_vec.iter(){	
-			let c_ref: uint = from_str(i.as_slice()).unwrap();
-	/* 		let c_ref :uint = match from_str(i.as_slice()){				
+			let c_ref: uint = i.as_slice().parse::<uint>().unwrap();
+	/* 		let c_ref :u32 = match from_str(i.as_slice()){				
 				Some(x) => x,
 				None =>  break;,
 			};  */
