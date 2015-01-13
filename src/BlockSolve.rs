3098c31ae73e326fba3346565583babf1015c56a
Jojoshua <Jojoshua@gmail.com>
2015-01-13 00:20:16 -0500

    Cargo

--- src/BlockSolve.rs
+++ src/BlockSolve.rs
@@ -1,16 +1,41 @@
+extern crate time;
+
 use std::io::File;
 use std::io::BufferedReader;
 
 use std::collections::HashSet;
 use std::collections::HashMap;
 
+use std::thread::Thread;
+use std::sync::mpsc::channel;
+
 #[derive(Hash, Eq, PartialEq, Show)]
 struct PMap {
     protein: String,
     set_ref: uint,
 }
+
+struct Timer<'a> {
+    name: &'a str,
+    start: f64,	
+}
+
+impl<'a> Timer<'a> {	
+    fn new<'a>(name: &'a str) -> Timer{
+		Timer{name: name, start: time::precise_time_s()}
+    }
+	
+	fn stop(&self) -> f64{
+		let diff  = time::precise_time_s() - self.start ;	
+		println!("{} - {}", self.name , diff );
+		diff	
+	}
+}
 	
 fn main(){
+	//let begin = time::precise_time_s();
+	//let end;
+		
 	let mut all_sets: HashMap<uint,HashSet<uint>> = HashMap::new();	
 	let mut p_map: HashSet<PMap> = HashSet::new();		
 	
@@ -21,14 +46,28 @@ fn main(){
 /* 	for a in main_map.iter(){	
 		println!("Original sequence {}", a );	
 	} */
-	
+	let t: Timer = Timer::new("round one");
 	let mut a_count: uint = 0;		
 	for (a_k, a_v) in main_map.iter(){		
 		a_count += 1;
+		println!("{}", a_count );
 		//println!("\n A: {}, {}", a_k , a_v );	
 		
-		for (b_k, b_v) in main_map.iter().skip(a_count){						
-			//println!(" B: {}, {}", b_k , b_v );
+				
+/*  		let mut b_range = main_map.iter().skip(a_count);
+		for (b_k, b_v) in b_range{
+			//println!(" Skip loop {}, {}", b_k , b_v );
+			
+			let intersection : HashSet<uint> = a_v.intersection(b_v).map(|&x| x).collect();
+			if intersection.len() > 1{
+				//println!("{} {} {}", a_k, b_k, intersection );
+				insert_p_map(a_k, &intersection, &mut p_map,&mut all_sets);	
+				insert_p_map(b_k, &intersection, &mut p_map,&mut all_sets);	
+			}
+		}  */
+	
+   	 	for (b_k, b_v) in main_map.iter().skip(a_count){						
+			//println!(" Orig Loop B: {}, {}", b_k , b_v );
 			
 			//Intersect the two protein sets
 		/* 	for x in a_v.intersection(b_v) {
@@ -37,22 +76,41 @@ fn main(){
 			let intersection : HashSet<uint> = a_v.intersection(b_v).map(|&x| x).collect();
 			if intersection.len() > 1{
 				//println!("{} {} {}", a_k, b_k, intersection );
-				insert_p_map(a_k, &intersection, &mut p_map,&mut all_sets);	
-				insert_p_map(b_k, &intersection, &mut p_map,&mut all_sets);	
+				//insert_p_map(a_k, &intersection, &mut p_map,&mut all_sets);	
+				//insert_p_map(b_k, &intersection, &mut p_map,&mut all_sets);	
 			}						
-		}						
+		}  				
+	}	
+	
+
+		
+		
+	if t.stop() < 0.8 {
+		//println!("\n Round One was fast {}", t.stop() );
+	/* 	for (a_k, a_v) in main_map.iter(){	
+			println!("\n{}, {}", a_k , a_v );	
+		} */
 	}
 	
+	let t: Timer = Timer::new("round two");
 	find_sub_sets(&all_sets,&mut p_map);
+	if t.stop() < 1.0 {
+		//println!("\n Round Two was fast {}", t.stop() );	
+	/* 	for (a_k, a_v) in main_map.iter(){	
+			println!("\n{}, {}", a_k , a_v );	
+		} */
+	}
 	
-	for n in p_map.iter(){
+	
+/* 	for n in p_map.iter(){
 		println!("P Map {} {}", n.protein, n.set_ref );
 	}
 	
 	for (k,v) in all_sets.iter(){
 		println!("All Set {} {}", k, v );	
-	}			
+	}	 */		
 	
+	let t: Timer = Timer::new("last loop");
 	//Print how many of each set there are
 	let mut unique_set_count: HashSet<uint> = HashSet::new();
 	for a in p_map.iter(){
@@ -69,9 +127,30 @@ fn main(){
 				proteins.insert(b.protein.to_string());
 			}
 		}		
-		println!("Set {} Count {} Proteins {}", a.set_ref ,count, proteins);
+		//println!("Set {} Count {} Proteins {}", a.set_ref ,count, proteins);
 	}
+	t.stop();
 	
+/* 	end = time::precise_time_s();
+	println!("Ran in {} S", end - begin ); */
+	
+}
+
+fn do_intersection(a_k: &String, a_v: HashSet<uint>, main_map: HashMap<String,HashSet<uint>>, all_sets: HashMap<uint,HashSet<uint>>, p_map: HashSet<PMap>, a_count: uint){
+
+	let (tx, rx) = channel();
+	Thread::spawn(move|| {
+		for (b_k, b_v) in main_map.iter().skip(a_count){						
+			//println!(" Orig Loop B: {}, {}", b_k , b_v );
+		
+			//Intersect the two protein sets
+			let intersection : HashSet<uint> = a_v.intersection(b_v).map(|&x| x).collect();
+			tx.send(intersection);								
+		}			
+	});
+	
+	println!("{}", rx.recv() );	
+
 }
 
  //Insert or Update master list of sets and return the reference number to the set
@@ -79,7 +158,7 @@ fn main(){
 	let ref_num;
 	
 	for (k,v) in all_sets.iter(){
-		if *v == *set{			
+		if *v == *set{
 			return *k;
 		}
 	}
@@ -128,8 +207,10 @@ fn add_sub_set(subset: &uint, superset: &uint, p_map: &mut HashSet<PMap>){
 
 
 fn load_input(main_map: &mut HashMap<String,HashSet<uint>>){
+	let t: Timer = Timer::new("load_input");
+	
 	// Create a path to the desired file
-    let path = Path::new("random_input.txt");
+    let path = Path::new("./input/input.txt");
 	
 	let mut file = BufferedReader::new(File::open(&path));
 	for line in file.lines().filter_map(|result| result.ok()) {		
@@ -162,10 +243,12 @@ fn load_input(main_map: &mut HashMap<String,HashSet<uint>>){
 		
 		main_map.insert(protein.to_string(),c_set);	
 	}
-		
+	
+	t.stop();
 }
  
 fn create_p_map(){
+	
 	let mut p_map = HashMap::new();
 	
  	let mut num_protein_sets = 5000i;
@@ -180,8 +263,8 @@ fn create_p_map(){
 		println!("{}",num_protein_sets);
 		num_protein_sets = num_protein_sets -1 ;
 	}
+	
 }
-
 
 fn infinite(){
 	loop {	
